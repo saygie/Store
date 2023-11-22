962d4655b5297700d71edca5ca393dee8c38c4a6
Ersel Saygı <erselsaygi@egm.gov.tr>
2023-11-22 18:46:02 +0300

    Initial commit

---
+++ Store/ViewComponents/VerticalSecondaryMenuViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Store.Models.Services;
+
+namespace Store.ViewComponents;
+
+public class VerticalSecondaryMenuViewComponent : ViewComponent
+{
+    private readonly ILogger<VerticalSecondaryMenuViewComponent> logger;
+    private readonly ICategoryService categoryService;
+    public VerticalSecondaryMenuViewComponent(ICategoryService categoryService, ILogger<VerticalSecondaryMenuViewComponent> logger)
+    {
+        this.categoryService = categoryService;
+        this.logger = logger;
+    }
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        var categories = await categoryService.List();
+        return View(categories);
+    }
+    //public async Task<IViewComponentResult> InvokeAsync()
+    //{
+    //    return View();
+    //}
+}
