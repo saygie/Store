02642377bd9188264ddf79ddb145c28f81601740
erselsaygi <erselsaygi@gmail.com>
2023-12-09 01:40:56 +0300

    initial commit

---
+++ Store/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Store.Controllers;
+
+public class AccountController : Controller
+{
+    private readonly ILogger<BasketController> logger;
+    private readonly UserManager<IdentityUser> userManager;
+    private readonly SignInManager<IdentityUser> signInManager;
+    public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager, ILogger<BasketController> logger)
+    {
+        this.userManager = userManager;
+        this.signInManager = signInManager;
+        this.logger = logger;
+    }
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
